4156e53767529c6cc2989efd75c43934e9ce7dda
Josef31 <josef310798@gmail.com>
2026-05-14 10:57:28 -0400

    primeros 5 ejercicios

---
+++ index.js
@@ -0,0 +1,15 @@
+//importar 
+import { saludar } from "./ej02.js"
+import { ej1 } from "./ej01.js";
+import { ej3 } from "./ej03.js";
+import { ej5 } from "./ej05.js";
+import { ej7 } from "./ej07.js";
+import { ej9 } from "./ej09.js";
+
+
+saludar();
+ej1();
+ej3();
+ej5();
+ej7();
+ej9();
